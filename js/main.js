b227f9f4cbe0cfda37589255f46bde37d4ab26d5
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-09-15 16:13:24 -0300

    Add link to logo

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ document.getElementById('mr-robot-play').onclick = function () {
     location.href = "/pages/mr-robot/index.html";
 
 }
+document.getElementsByClassName('logo')[0].onclick = function () {
+    location.href = "/";
+}
 
 
 
