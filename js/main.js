0fb3487e07ab17f88ae73ea9246359bcec2eda33
Raphael Fontoura <raphael.fontoura@gmail.com>
2021-09-15 16:41:08 -0300

    fix link logo

--- js/main.js
+++ js/main.js
@@ -3,10 +3,4 @@ document.getElementById('mr-robot-play').onclick = function () {
     location.href = "/pages/mr-robot/index.html";
 
 }
-document.getElementsByClassName('logo')[0].onclick = function () {
-    location.href = "/";
-}
-
-
-
 
